3e1129b015abd0af324eb9ec2484d02d057d75a9
Cash Costello <cash.costello@gmail.com>
2022-02-19 10:31:18 -0500

    adds django metadata classifier

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
+        "Framework :: Django",
     ]
 )
